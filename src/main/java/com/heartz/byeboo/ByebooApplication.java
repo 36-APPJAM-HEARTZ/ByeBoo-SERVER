f90a29cca1633716fa48db1debd6d336216eb576
ssangbaam <tkdqja2196@naver.com>
2025-07-01 02:06:19 +0900

    setting: 프로젝트 초기 세팅

---
+++ src/main/java/com/heartz/byeboo/ByebooApplication.java
@@ -0,0 +1,13 @@
+package com.heartz.byeboo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByebooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByebooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heartz/byeboo/ByebooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heartz.byeboo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByebooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
