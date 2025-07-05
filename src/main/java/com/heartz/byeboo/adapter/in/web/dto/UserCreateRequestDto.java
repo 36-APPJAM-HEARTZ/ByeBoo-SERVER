84e66e57c36d5cfc0127604bc07aa84e098550be
ssangbaam <tkdqja2196@naver.com>
2025-07-06 00:11:35 +0900

    feat: 유저 추가 API 예외 처리를 위한 dto 변수 타입 변경

--- src/main/java/com/heartz/byeboo/adapter/in/web/dto/UserCreateRequestDto.java
+++ src/main/java/com/heartz/byeboo/adapter/in/web/dto/UserCreateRequestDto.java
@@ -1,9 +1,7 @@
 package com.heartz.byeboo.adapter.in.web.dto;
 
-import com.heartz.byeboo.domain.type.EQuestStyle;
-
 public record UserCreateRequestDto(
         String name,
-        EQuestStyle questStyle
+        String questStyle
 ) {
 }
