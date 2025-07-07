086fa993fe15a9bce05791e81bb45eccd5cfb768
ssangbaam <tkdqja2196@naver.com>
2025-07-07 18:05:08 +0900

    feat: 유저-여정 repository 추가

--- src/main/java/com/heartz/byeboo/adapter/out/persistence/repository/UserJourneyRepository.java
+++ src/main/java/com/heartz/byeboo/adapter/out/persistence/repository/UserJourneyRepository.java
@@ -3,5 +3,8 @@ package com.heartz.byeboo.adapter.out.persistence.repository;
 import com.heartz.byeboo.adapter.out.persistence.entity.UserJourneyEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserJourneyRepository extends JpaRepository<UserJourneyEntity, Long> {
+    List<UserJourneyEntity> findAllByUserId(Long userId);
 }
