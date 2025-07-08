3f28422b2b1bd15f53721d1c60125b7a6352d78c
ssangbaam <tkdqja2196@naver.com>
2025-07-08 18:06:51 +0900

    feat: 완료된 퀘스트 갯수 조회 로직 구현

--- src/main/java/com/heartz/byeboo/application/port/in/UserUseCase.java
+++ src/main/java/com/heartz/byeboo/application/port/in/UserUseCase.java
@@ -1,8 +1,10 @@
 package com.heartz.byeboo.application.port.in;
 
+import com.heartz.byeboo.adapter.in.web.dto.response.HomeCountResponseDto;
 import com.heartz.byeboo.adapter.in.web.dto.response.UserCreateResponseDto;
 import com.heartz.byeboo.adapter.in.web.dto.response.UserJourneyResponseDto;
 import com.heartz.byeboo.adapter.in.web.dto.response.UserNameResponseDto;
+import com.heartz.byeboo.application.command.HomeCountCommand;
 import com.heartz.byeboo.application.command.UserCreateCommand;
 import com.heartz.byeboo.application.command.UserJourneyCommand;
 import com.heartz.byeboo.application.command.UserNameCommand;
@@ -11,4 +13,5 @@ public interface UserUseCase {
     UserCreateResponseDto createUser(UserCreateCommand userCreateCommand);
     UserNameResponseDto getUserName(UserNameCommand userNameCommand);
     UserJourneyResponseDto getUserJourney(UserJourneyCommand userJourneyCommand);
+    HomeCountResponseDto getHomeCount(HomeCountCommand homeCountCommand);
 }
--- src/main/java/com/heartz/byeboo/application/service/UserService.java
+++ src/main/java/com/heartz/byeboo/application/service/UserService.java
@@ -1,24 +1,28 @@
 package com.heartz.byeboo.application.service;
 
+import com.heartz.byeboo.adapter.in.web.dto.response.HomeCountResponseDto;
 import com.heartz.byeboo.adapter.in.web.dto.response.UserCreateResponseDto;
 import com.heartz.byeboo.adapter.in.web.dto.response.UserJourneyResponseDto;
 import com.heartz.byeboo.adapter.in.web.dto.response.UserNameResponseDto;
+import com.heartz.byeboo.application.command.HomeCountCommand;
 import com.heartz.byeboo.application.command.UserCreateCommand;
 import com.heartz.byeboo.application.command.UserJourneyCommand;
 import com.heartz.byeboo.application.command.UserNameCommand;
 import com.heartz.byeboo.application.port.in.UserUseCase;
-import com.heartz.byeboo.application.port.out.CreateUserJourneyPort;
-import com.heartz.byeboo.application.port.out.CreateUserPort;
-import com.heartz.byeboo.application.port.out.RetrieveUserJourneyPort;
-import com.heartz.byeboo.application.port.out.RetrieveUserPort;
+import com.heartz.byeboo.application.port.out.*;
+import com.heartz.byeboo.core.exception.CustomException;
+import com.heartz.byeboo.domain.exception.UserQuestErrorCode;
+import com.heartz.byeboo.domain.model.Quest;
 import com.heartz.byeboo.domain.model.User;
 import com.heartz.byeboo.domain.model.UserJourney;
+import com.heartz.byeboo.domain.model.UserQuest;
 import com.heartz.byeboo.domain.type.EJourneyStatus;
 import com.heartz.byeboo.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -28,6 +32,8 @@ public class UserService implements UserUseCase {
     private final CreateUserJourneyPort createUserJourneyPort;
     private final RetrieveUserPort retrieveUserPort;
     private final RetrieveUserJourneyPort retrieveUserJourneyPort;
+    private final RetrieveQuestPort retrieveQuestPort;
+    private final RetrieveUserQuestPort retrieveUserQuestPort;
 
     @Override
     @Transactional
@@ -70,5 +76,30 @@ public class UserService implements UserUseCase {
                     userJourney.getJourney().getLabel(),
                     null
             );
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public HomeCountResponseDto getHomeCount(HomeCountCommand homeCountCommand) {
+        User currentUser = retrieveUserPort.getUserById(homeCountCommand.getId());
+        UserJourney ongoingUserJourney = retrieveUserJourneyPort.getUserJourneyByUser(currentUser);
+
+        if(currentUser.getCurrentNumber() == 0)
+            throw new CustomException(UserQuestErrorCode.NOT_FOUND_ONGOING_USER_QUEST);
+
+        Quest quest = retrieveQuestPort.getQuestByJourneyAndStepNumber(
+                ongoingUserJourney.getJourney(),
+                currentUser.getCurrentNumber()
+        );
+        UserQuest recentUserQuest = retrieveUserQuestPort.getUserQuestByUserAndQuest(currentUser, quest);
+        Boolean todayCompleted = Boolean.FALSE;
+
+        if(recentUserQuest == null)
+            return HomeCountResponseDto.of(todayCompleted, 0L);
+
+        if(recentUserQuest.getCreatedDate().plusDays(1).isAfter(LocalDateTime.now()))
+            todayCompleted = Boolean.TRUE;
+
+        return HomeCountResponseDto.of(todayCompleted, currentUser.getCurrentNumber() - 1);
     }
 }
