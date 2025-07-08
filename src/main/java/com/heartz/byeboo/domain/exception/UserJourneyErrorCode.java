a821f8b14eb2cee2fc3c74ba05e8a276db1cc045
ssangbaam <tkdqja2196@naver.com>
2025-07-08 22:17:18 +0900

    feat: 현재 시작전, 진행중인 여정 예외처리 추가

--- src/main/java/com/heartz/byeboo/domain/exception/UserJourneyErrorCode.java
+++ src/main/java/com/heartz/byeboo/domain/exception/UserJourneyErrorCode.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum UserJourneyErrorCode implements ErrorCode {
     NOT_FOUND_ONGOING_USER_JOURNEY(HttpStatus.NOT_FOUND, "현재 진행중인 여정이 없습니다."),
+    CONFLICT_USER_JOURNEY_STATUS(HttpStatus.CONFLICT, "현재 진행중인 여정이 없습니다.")
     ;
 
     private final HttpStatus status;
