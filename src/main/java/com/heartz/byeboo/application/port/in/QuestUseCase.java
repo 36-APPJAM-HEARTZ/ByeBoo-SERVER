a7bf88bd7462e360b1ee16d55f3eaeb3eeff0147
jeong724 <oliveyou724@gmail.com>
2025-07-09 01:45:42 +0900

    feat : controller 이름 수정 quest -> userquest

--- src/main/java/com/heartz/byeboo/application/port/in/QuestUseCase.java
+++ src/main/java/com/heartz/byeboo/application/port/in/QuestUseCase.java
@@ -1,15 +1,8 @@
 package com.heartz.byeboo.application.port.in;
 
-import com.heartz.byeboo.adapter.in.web.dto.SignedUrlResponseDto;
-import com.heartz.byeboo.adapter.in.web.dto.response.QuestDetailResponseDto;
-import com.heartz.byeboo.application.command.ActiveQuestCreateCommand;
-import com.heartz.byeboo.application.command.QuestDetailCommand;
-import com.heartz.byeboo.application.command.RecordingQuestCreateCommand;
-import com.heartz.byeboo.application.command.SignedUrlCreateCommand;
+import com.heartz.byeboo.adapter.in.web.dto.response.TipListResponseDto;
+import com.heartz.byeboo.application.command.QuestTipCommand;
 
 public interface QuestUseCase {
-    void createRecordingQuest(RecordingQuestCreateCommand command);
-    void createActiveQuest(ActiveQuestCreateCommand command);
-    SignedUrlResponseDto getSignedUrl(SignedUrlCreateCommand command);
-    QuestDetailResponseDto getDetailQuest(QuestDetailCommand command);
+    TipListResponseDto getQuestTip(QuestTipCommand command);
 }
