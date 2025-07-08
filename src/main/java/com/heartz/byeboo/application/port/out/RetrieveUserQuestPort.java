3d7b320f12b8a3fe9d14962e1c165c079bf43d1c
ssangbaam <tkdqja2196@naver.com>
2025-07-08 18:02:45 +0900

    feat: UserQuest, Quest 조회 port 구현

--- src/main/java/com/heartz/byeboo/application/port/out/RetrieveUserQuestPort.java
+++ src/main/java/com/heartz/byeboo/application/port/out/RetrieveUserQuestPort.java
@@ -4,8 +4,6 @@ import com.heartz.byeboo.domain.model.Quest;
 import com.heartz.byeboo.domain.model.User;
 import com.heartz.byeboo.domain.model.UserQuest;
 
-import java.util.List;
-
 public interface RetrieveUserQuestPort {
     UserQuest getUserQuestByUserAndQuest(User user, Quest quest);
 }
