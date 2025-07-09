c37defa7cb5cc5d5ab54ce6fba17056acc8e0fc6
ssangbaam <tkdqja2196@naver.com>
2025-07-09 21:30:25 +0900

    feat: 퀘스트 전체 조회 로직 구현

--- src/main/java/com/heartz/byeboo/application/service/QuestService.java
+++ src/main/java/com/heartz/byeboo/application/service/QuestService.java
@@ -1,17 +1,27 @@
 package com.heartz.byeboo.application.service;
 
-import com.heartz.byeboo.adapter.in.web.dto.response.TipListResponseDto;
-import com.heartz.byeboo.adapter.in.web.dto.response.TipResponseDto;
+import com.heartz.byeboo.adapter.in.web.dto.response.*;
+import com.heartz.byeboo.application.command.AllQuestCommand;
 import com.heartz.byeboo.application.command.QuestTipCommand;
 import com.heartz.byeboo.application.port.in.QuestUseCase;
 import com.heartz.byeboo.application.port.out.RetrieveQuestPort;
 import com.heartz.byeboo.application.port.out.RetrieveTipPort;
+import com.heartz.byeboo.application.port.out.RetrieveUserJourneyPort;
+import com.heartz.byeboo.application.port.out.RetrieveUserPort;
 import com.heartz.byeboo.domain.model.Quest;
+import com.heartz.byeboo.domain.model.User;
+import com.heartz.byeboo.domain.model.UserJourney;
+import com.heartz.byeboo.domain.type.EJourneyStatus;
+import com.heartz.byeboo.domain.type.EStep;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -19,6 +29,8 @@ public class QuestService implements QuestUseCase {
 
     private final RetrieveTipPort retrieveTipPort;
     private final RetrieveQuestPort retrieveQuestPort;
+    private final RetrieveUserPort retrieveUserPort;
+    private final RetrieveUserJourneyPort retrieveUserJourneyPort;
 
     @Override
     @Transactional(readOnly = true)
@@ -28,5 +40,49 @@ public class QuestService implements QuestUseCase {
                 .stream().map(TipResponseDto::from).toList();
 
         return TipListResponseDto.of(findQuest, tips);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public AllQuestResponseDto getAllQuest(AllQuestCommand allQuestCommand) {
+        User currentUser = retrieveUserPort.getUserById(allQuestCommand.getUserId());
+        UserJourney userJourney = retrieveUserJourneyPort.getUserJourneyByUserAndJourney(
+                currentUser,
+                allQuestCommand.getJourney()
+        );
+        List<Quest> quests = retrieveQuestPort.getALlQuestByJourney(allQuestCommand.getJourney());
+        Map<EStep, List<Quest>> stepGroupQuests
+                = quests.stream()
+                .collect(Collectors.groupingBy(Quest::getStep));
+        List<StepResponseDto> stepResponses
+                = stepGroupQuests.entrySet().stream()
+                .sorted(Comparator.comparingInt(
+                        stepGroupQuest -> stepGroupQuest.getKey().getStepNumber())
+                )
+                .map(stepListEntry -> StepResponseDto.of(
+                        stepListEntry.getKey().getStepNumber(),
+                        stepListEntry.getKey(),
+                        stepListEntry.getValue().stream()
+                                .map(AllQuestDetailResponseDto::from)
+                                .toList())
+                )
+                .toList();
+
+        if(userJourney.getJourneyStatus().equals(EJourneyStatus.COMPLETED))
+            return AllQuestResponseDto.of(
+                    userJourney.getJourneyStart().toString() + " ~ " + userJourney.getJourneyEnd().toString(),
+                    null,
+                    true,
+                    stepResponses
+            );
+
+        long progressPeriod = LocalDate.now().toEpochDay() - userJourney.getJourneyStart().toEpochDay() + 1;
+
+        return AllQuestResponseDto.of(
+                Long.toString(progressPeriod),
+                currentUser.getCurrentNumber(),
+                false,
+                stepResponses
+        );
     }
 }
