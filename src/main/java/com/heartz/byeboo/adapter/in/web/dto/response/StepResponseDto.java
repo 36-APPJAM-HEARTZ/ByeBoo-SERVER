b08e988d0a5865ed62b5c5b85c194d2dc5df1535
ssangbaam <tkdqja2196@naver.com>
2025-07-09 21:25:08 +0900

    feat: 전체 퀘스트 조회 responseDto 추가

---
+++ src/main/java/com/heartz/byeboo/adapter/in/web/dto/response/StepResponseDto.java
@@ -0,0 +1,15 @@
+package com.heartz.byeboo.adapter.in.web.dto.response;
+
+import com.heartz.byeboo.domain.type.EStep;
+
+import java.util.List;
+
+public record StepResponseDto(
+        Integer stepNumber,
+        String step,
+        List<AllQuestDetailResponseDto> quests
+) {
+    public static StepResponseDto of(Integer stepNumber, EStep step, List<AllQuestDetailResponseDto> quests) {
+        return new StepResponseDto(stepNumber, step.getLabel(), quests);
+    }
+}
