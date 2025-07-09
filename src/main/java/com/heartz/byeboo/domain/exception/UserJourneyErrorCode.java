5bd0b2cb1d9c3754807ccf6bd0e9b4241ba62903
ssangbaam <tkdqja2196@naver.com>
2025-07-09 21:32:12 +0900

    feat: 여정 예외처리 추가

--- src/main/java/com/heartz/byeboo/domain/exception/UserJourneyErrorCode.java
+++ src/main/java/com/heartz/byeboo/domain/exception/UserJourneyErrorCode.java
@@ -9,7 +9,11 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum UserJourneyErrorCode implements ErrorCode {
     NOT_FOUND_ONGOING_USER_JOURNEY(HttpStatus.NOT_FOUND, "현재 진행중인 여정이 없습니다."),
-    CONFLICT_USER_JOURNEY_STATUS(HttpStatus.CONFLICT, "현재 진행중인 여정이 없습니다.")
+    CONFLICT_USER_JOURNEY_STATUS(HttpStatus.CONFLICT, "현재 진행중인 여정이 없습니다."),
+    CONFLICT_USER_JOURNEY(HttpStatus.CONFLICT, "이미 진행 완료된 여정입니다."),
+    INVALID_USER_JOURNEY(HttpStatus.BAD_REQUEST, "올바르지 않은 여정 형식입니다."),
+    INVALID_JOURNEY_TYPE(HttpStatus.BAD_REQUEST, "올바르지 않은 여정입니다."),
+    NOT_FOUND_USER_JOURNEY(HttpStatus.NOT_FOUND, "찾고자 하는 여정이 존재하지 않습니다."),
     ;
 
     private final HttpStatus status;
