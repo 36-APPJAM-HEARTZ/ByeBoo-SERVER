e2850360349fc31643f2a1eac2ebc79022193189
jeong724 <oliveyou724@gmail.com>
2025-07-09 16:52:55 +0900

    feat : 이미 진행 완료한 여정을 시작하려할때 에러 코드 추가

--- src/main/java/com/heartz/byeboo/domain/exception/UserJourneyErrorCode.java
+++ src/main/java/com/heartz/byeboo/domain/exception/UserJourneyErrorCode.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum UserJourneyErrorCode implements ErrorCode {
     NOT_FOUND_ONGOING_USER_JOURNEY(HttpStatus.NOT_FOUND, "현재 진행중인 여정이 없습니다."),
-    CONFLICT_USER_JOURNEY_STATUS(HttpStatus.CONFLICT, "현재 진행중인 여정이 없습니다.")
+    CONFLICT_USER_JOURNEY_STATUS(HttpStatus.CONFLICT, "현재 진행중인 여정이 없습니다."),
+    CONFLICT_USER_JOURNEY(HttpStatus.CONFLICT, "이미 진행 완료된 여정입니다."),
     ;
 
     private final HttpStatus status;
