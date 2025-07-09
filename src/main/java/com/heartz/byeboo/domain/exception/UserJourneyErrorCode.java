5dcf373f98cd2f77f4eecd45f598a382e8d54955
jeong724 <oliveyou724@gmail.com>
2025-07-09 17:49:29 +0900

    feat : 여정이 이미 시작 상태일때 에러 코드 추가, 형식이 맞지 않을때 에러 코드 추가

--- src/main/java/com/heartz/byeboo/domain/exception/UserJourneyErrorCode.java
+++ src/main/java/com/heartz/byeboo/domain/exception/UserJourneyErrorCode.java
@@ -11,7 +11,7 @@ public enum UserJourneyErrorCode implements ErrorCode {
     NOT_FOUND_ONGOING_USER_JOURNEY(HttpStatus.NOT_FOUND, "현재 진행중인 여정이 없습니다."),
     CONFLICT_USER_JOURNEY_STATUS(HttpStatus.CONFLICT, "현재 진행중인 여정이 없습니다."),
     CONFLICT_USER_JOURNEY(HttpStatus.CONFLICT, "이미 진행 완료된 여정입니다."),
-    ;
+    INVALID_USER_JOURNEY(HttpStatus.BAD_REQUEST, "올바르지 않은 여정 형식입니다.");
 
     private final HttpStatus status;
     private final String message;
