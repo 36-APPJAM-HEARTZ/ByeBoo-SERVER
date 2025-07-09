0282200b48826394cc81e0f768db410201ab9280
ssangbaam <tkdqja2196@naver.com>
2025-07-09 21:32:46 +0900

    refac: 메서드명 수정에 따른 리팩토링

--- src/main/java/com/heartz/byeboo/application/port/out/RetrieveUserJourneyPort.java
+++ src/main/java/com/heartz/byeboo/application/port/out/RetrieveUserJourneyPort.java
@@ -2,7 +2,12 @@ package com.heartz.byeboo.application.port.out;
 
 import com.heartz.byeboo.domain.model.User;
 import com.heartz.byeboo.domain.model.UserJourney;
+import com.heartz.byeboo.domain.type.EJourney;
+
+import java.util.List;
 
 public interface RetrieveUserJourneyPort {
     UserJourney getOngoingUserJourneyByUser(User user);
+    List<UserJourney> getJourneysByUser(User user);
+    UserJourney getUserJourneyByUserAndJourney(User user, EJourney journey);
 }
