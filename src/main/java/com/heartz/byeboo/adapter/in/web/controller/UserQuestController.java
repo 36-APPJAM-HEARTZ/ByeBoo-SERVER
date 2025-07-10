bf4337772ffad3820f51e3cf7e0ae89c325886ff
jeong724 <oliveyou724@gmail.com>
2025-07-10 23:39:45 +0900

    docs : 스웨거 설명 추가

--- src/main/java/com/heartz/byeboo/adapter/in/web/controller/UserQuestController.java
+++ src/main/java/com/heartz/byeboo/adapter/in/web/controller/UserQuestController.java
@@ -10,6 +10,7 @@ import com.heartz.byeboo.application.command.*;
 import com.heartz.byeboo.application.command.userquest.*;
 import com.heartz.byeboo.application.port.in.UserQuestUseCase;
 import com.heartz.byeboo.core.common.BaseResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,6 +21,10 @@ public class UserQuestController {
 
     private final UserQuestUseCase userQuestUseCase;
 
+    @Operation(
+            summary = "퀘스트 답변 저장(질문/답변형)",
+            description = "질문/답변형 퀘스트에 대한 답변을 저장하는 API입니다."
+    )
     @PostMapping("/{questId}/recording")
     public BaseResponse<Void> createRecordingQuest(
             @RequestHeader final Long userId,
@@ -30,6 +35,10 @@ public class UserQuestController {
         return BaseResponse.success(null);
     }
 
+    @Operation(
+            summary = "퀘스트 답변 저장(행동형)",
+            description = "퀘스트에 대한 답변을 저장하는 API입니다. (사진 제외, 텍스트만!)"
+    )
     @PostMapping("/{questId}/active")
     public BaseResponse<Void> createActiveQuest(
             @RequestHeader final Long userId,
@@ -40,6 +49,11 @@ public class UserQuestController {
         return BaseResponse.success(null);
     }
 
+    @Operation(
+            summary = "Signed URL 요청 (행동형 퀘스트)",
+            description = "사진 첨부 (+ 버튼) 눌렀을때 Presigned Url을 요청하는 API 입니다."
+                    +"클라이언트는 발급받은 presigned url에 PUT 요청을 해서 이미지를 업로드하면 됩니다. (완료하기 버튼 누르면 업로드 해주세요)"
+    )
     @PostMapping("/images/signed-url")
     public BaseResponse<SignedUrlResponseDto> createSignedUrl(
             @RequestHeader final Long userId,
@@ -49,6 +63,10 @@ public class UserQuestController {
         return BaseResponse.success(userQuestUseCase.getSignedUrl(command));
     }
 
+    @Operation(
+            summary = "작성된 퀘스트 상세 조회",
+            description = "기록한 퀘스트를 조회하는 API입니다."
+    )
     @GetMapping("/answer/{questId}")
     public BaseResponse<UserQuestDetailResponseDto> getDetailQuest(
             @RequestHeader final Long userId,
@@ -59,6 +77,10 @@ public class UserQuestController {
         return BaseResponse.success(userQuestUseCase.getDetailQuest(command));
     }
 
+    @Operation(
+            summary = "완료된 퀘스트 여정 조회",
+            description = "완료된 여정을 조회하는 API입니다."
+    )
     @GetMapping("/journey")
     public BaseResponse<JourneyListResponseDto> getCompletedJourney(
             @RequestHeader final Long userId
@@ -67,6 +89,10 @@ public class UserQuestController {
         return BaseResponse.success(userQuestUseCase.getCompletedJourney(command));
     }
 
+    @Operation(
+            summary = "새로운 여정 시작하기",
+            description = "새로운 여정을 눌렀을 때 보내야하는 API입니다."
+    )
     @PostMapping("/journey")
     public BaseResponse<Void> createJourney(
             @RequestHeader final Long userId,
