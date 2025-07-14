37e9805f9d014d9dc209fb6fd263dada6956a411
ssangbaam <tkdqja2196@naver.com>
2025-07-14 16:17:03 +0900

    del: 사용하지 않는 dto 클래스 제거

--- src/main/java/com/heartz/byeboo/adapter/in/web/dto/request/HomeCountRequestDto.java
+++
@@ -1,9 +0,0 @@
-package com.heartz.byeboo.adapter.in.web.dto.request;
-
-public record HomeCountRequestDto(
-        Long id
-) {
-    public static HomeCountRequestDto of(Long id) {
-        return new HomeCountRequestDto(id);
-    }
-}
