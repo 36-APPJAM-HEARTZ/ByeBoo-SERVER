8be722332e87fba4771703c9c9b8f4d5b5505106
ssangbaam <tkdqja2196@naver.com>
2025-08-12 18:44:01 +0900

    feat: 유저 이름 수정 예외처리 추가

--- src/main/java/com/heartz/byeboo/application/command/user/UserNameUpdateCommand.java
+++ src/main/java/com/heartz/byeboo/application/command/user/UserNameUpdateCommand.java
@@ -1,6 +1,9 @@
 package com.heartz.byeboo.application.command.user;
 
 import com.heartz.byeboo.adapter.in.web.dto.request.UserNameUpdateRequestDto;
+import com.heartz.byeboo.core.exception.CustomException;
+import com.heartz.byeboo.domain.exception.UserErrorCode;
+import com.heartz.byeboo.utils.TextUtil;
 import lombok.*;
 
 @Builder
@@ -12,9 +15,19 @@ public class UserNameUpdateCommand {
     private String name;
 
     public static UserNameUpdateCommand of(Long id, UserNameUpdateRequestDto userNameUpdateRequestDto){
+        validateUserName(userNameUpdateRequestDto.name());
+
         return UserNameUpdateCommand.builder()
                 .id(id)
                 .name(userNameUpdateRequestDto.name())
                 .build();
+    }
+
+    private static void validateUserName(String name){
+        if(TextUtil.lengthWithEmoji(name) < 2)
+            throw new CustomException(UserErrorCode.USER_NAME_TOO_SHORT);
+
+        if(TextUtil.lengthWithEmoji(name) > 5)
+            throw new CustomException(UserErrorCode.USER_NAME_TOO_LONG);
     }
 }
