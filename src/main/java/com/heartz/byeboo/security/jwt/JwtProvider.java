19404e1e9016ae5ffe4ad1e6cc36e7acc2f49d34
jeong724 <oliveyou724@gmail.com>
2025-08-19 23:06:37 +0900

    feat: Jwt 객체 생성 토큰 생성, 유효성 검증

---
+++ src/main/java/com/heartz/byeboo/security/jwt/JwtProvider.java
@@ -0,0 +1,31 @@
+package com.heartz.byeboo.security.jwt;
+
+import io.jsonwebtoken.JwtParser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class JwtProvider {
+
+    private final JwtGenerator jwtGenerator;
+
+    public Token issueTokens(final Long userId, final String role) {
+        return Token.of(generateAccessToken(userId, role), generateRefreshToken(userId, role));
+    }
+
+    private String generateAccessToken(final Long userId, final String role) {
+        return jwtGenerator.generateToken(userId, role, true);
+    }
+
+    private String generateRefreshToken(final Long userId, final String role) {
+        return jwtGenerator.generateToken(userId, role, false);
+    }
+
+    public Long getSubject(final String token) {
+        JwtParser jwtParser = jwtGenerator.getJwtParser();
+        return Long.valueOf(jwtParser.parseClaimsJws(token)
+                .getBody()
+                .getSubject());
+    }
+}
