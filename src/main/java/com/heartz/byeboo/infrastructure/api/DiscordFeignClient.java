1235f77e9cd308303460622c6ef727cf9f292e3f
jeong724 <oliveyou724@gmail.com>
2025-08-19 23:05:58 +0900

    feat: feign 세팅 + 기존 feign 클래스 이름 변경

--- src/main/java/com/heartz/byeboo/infrastructure/api/DiscordFeignClient.java
+++ src/main/java/com/heartz/byeboo/infrastructure/api/DiscordFeignClient.java
@@ -1,7 +1,7 @@
 package com.heartz.byeboo.infrastructure.api;
 
-import com.heartz.byeboo.config.DiscordFeignConfig;
-import com.heartz.byeboo.infrastructure.dto.DiscordMessageDto;
+import com.heartz.byeboo.config.FeignConfig;
+import com.heartz.byeboo.infrastructure.dto.discord.DiscordMessageDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 @FeignClient(
         name = "discord-client",
         url = "${logging.discord.webhook-url}",
-        configuration = DiscordFeignConfig.class)
-public interface DiscordClient {
+        configuration = FeignConfig.class)
+public interface DiscordFeignClient {
 
     @PostMapping()
     void sendAlarm(@RequestBody DiscordMessageDto message);
