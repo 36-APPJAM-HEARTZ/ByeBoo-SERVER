05daf763826ab6c83402e77c8bdd210734060e4b
jeong724 <oliveyou724@gmail.com>
2025-08-19 23:02:23 +0900

    feat: 유저 엔티티 수정

--- src/main/java/com/heartz/byeboo/adapter/out/persistence/entity/UserEntity.java
+++ src/main/java/com/heartz/byeboo/adapter/out/persistence/entity/UserEntity.java
@@ -1,6 +1,8 @@
 package com.heartz.byeboo.adapter.out.persistence.entity;
 
+import com.heartz.byeboo.domain.type.EPlatform;
 import com.heartz.byeboo.domain.type.EQuestStyle;
+import com.heartz.byeboo.domain.type.ERole;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -17,29 +19,46 @@ public class UserEntity extends BaseEntity{
     @Column(name = "id", nullable = false)
     private Long id;
 
-    @Column(name = "name", nullable = false)
+    @Column(name = "name")
     private String name;
 
     @Enumerated(EnumType.STRING)
-    @Column(name = "quest_style", nullable = false, length = 50)
+    @Column(name = "quest_style", length = 50)
     private EQuestStyle questStyle;
 
     @Column(name = "current_number")
     private Long currentNumber;
 
+    @Column(name = "serial_id", nullable = false, unique = true, updatable = false)
+    private String serialId;
+
+    @Column(name = "platform", nullable = false, updatable = false)
+    @Enumerated(EnumType.STRING)
+    private EPlatform platform;
+
+    @Column(name = "role", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private ERole role;
+
     @Builder
-    public UserEntity(Long id, String name, EQuestStyle questStyle, Long currentNumber) {
+    public UserEntity(Long id, String name, EQuestStyle questStyle, Long currentNumber,EPlatform platform, ERole role, String serialId) {
         this.id = id;
         this.name = name;
         this.questStyle = questStyle;
         this.currentNumber = currentNumber;
+        this.platform = platform;
+        this.role = role;
+        this.serialId = serialId;
     }
 
-    public static UserEntity create(String name, EQuestStyle questStyle, Long currentNumber) {
+    public static UserEntity create(String name, EQuestStyle questStyle, Long currentNumber, EPlatform platform, ERole role, String serialId) {
         return UserEntity.builder()
                 .name(name)
                 .questStyle(questStyle)
                 .currentNumber(currentNumber)
+                .platform(platform)
+                .serialId(serialId)
+                .role(role)
                 .build();
     }
 
