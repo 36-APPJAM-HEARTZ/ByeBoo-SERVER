11ab63f824f23f996bfbe3c790c75b3c2ab9d933
jeong724 <oliveyou724@gmail.com>
2025-08-19 23:02:28 +0900

    feat: 유저 도메인 수정

--- src/main/java/com/heartz/byeboo/domain/model/User.java
+++ src/main/java/com/heartz/byeboo/domain/model/User.java
@@ -1,6 +1,8 @@
 package com.heartz.byeboo.domain.model;
 
+import com.heartz.byeboo.domain.type.EPlatform;
 import com.heartz.byeboo.domain.type.EQuestStyle;
+import com.heartz.byeboo.domain.type.ERole;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -11,13 +13,19 @@ public class User {
     private String name;
     private EQuestStyle questStyle;
     private Long currentNumber;
+    private String serialId;
+    private EPlatform platform;
+    private ERole role;
 
-    public static User of(Long id, String name, EQuestStyle questStyle, Long currentNumber) {
+    public static User of(Long id, String name, EQuestStyle questStyle, Long currentNumber, EPlatform platform, ERole role, String serialId) {
         return User.builder()
                 .id(id)
                 .name(name)
                 .questStyle(questStyle)
                 .currentNumber(currentNumber)
+                .role(role)
+                .serialId(serialId)
+                .platform(platform)
                 .build();
     }
 
