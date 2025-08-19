8fbadda5b3c75102c42502e3505273ff0396de3b
jeong724 <oliveyou724@gmail.com>
2025-08-19 23:02:07 +0900

    feat: repository 메서드 추가

--- src/main/java/com/heartz/byeboo/adapter/out/persistence/repository/UserRepository.java
+++ src/main/java/com/heartz/byeboo/adapter/out/persistence/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package com.heartz.byeboo.adapter.out.persistence.repository;
 
 import com.heartz.byeboo.adapter.out.persistence.entity.UserEntity;
+import com.heartz.byeboo.domain.type.EPlatform;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    UserEntity findByPlatformAndSerialId(EPlatform platform, String serialId);
 }
