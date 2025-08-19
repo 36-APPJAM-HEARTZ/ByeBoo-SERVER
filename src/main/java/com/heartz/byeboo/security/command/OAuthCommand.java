9068d3be2245325bd1c8fac3495326f1fec85f31
jeong724 <oliveyou724@gmail.com>
2025-08-19 23:11:43 +0900

    file: 컨트롤러 커맨드 객체

---
+++ src/main/java/com/heartz/byeboo/security/command/OAuthCommand.java
@@ -0,0 +1,27 @@
+package com.heartz.byeboo.security.command;
+
+import com.heartz.byeboo.core.exception.CustomException;
+import com.heartz.byeboo.domain.exception.AuthErrorCode;
+import com.heartz.byeboo.domain.type.EPlatform;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class OAuthCommand {
+
+    private EPlatform platform;
+    private String token;
+
+    public static OAuthCommand of(String platform, String token){
+        try {
+            return OAuthCommand.builder()
+                    .platform(EPlatform.valueOf(platform))
+                    .token(token)
+                    .build();
+        } catch (IllegalArgumentException e){
+            throw new CustomException(AuthErrorCode.INVALID_PLATFORM);
+        }
+    }
+
+}
