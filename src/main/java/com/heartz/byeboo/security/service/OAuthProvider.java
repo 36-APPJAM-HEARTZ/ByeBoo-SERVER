c863b146d16a20933d9f224181f3738aa28d3ed5
jeong724 <oliveyou724@gmail.com>
2025-08-19 23:11:33 +0900

    file: oauth 인터페이스

---
+++ src/main/java/com/heartz/byeboo/security/service/OAuthProvider.java
@@ -0,0 +1,9 @@
+package com.heartz.byeboo.security.service;
+
+import com.heartz.byeboo.infrastructure.dto.SocialInfoResponse;
+
+
+public interface OAuthProvider {
+
+    SocialInfoResponse getUserInfo(final String providerToken);
+}
