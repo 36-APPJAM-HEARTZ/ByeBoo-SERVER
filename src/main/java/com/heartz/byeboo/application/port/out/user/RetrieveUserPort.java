3a1c93a9656303962422f1f55aa9b408675d693b
jeong724 <oliveyou724@gmail.com>
2025-08-19 23:09:41 +0900

    feat: user 생성, 변환 매퍼 메서드 추가

--- src/main/java/com/heartz/byeboo/application/port/out/user/RetrieveUserPort.java
+++ src/main/java/com/heartz/byeboo/application/port/out/user/RetrieveUserPort.java
@@ -1,7 +1,11 @@
 package com.heartz.byeboo.application.port.out.user;
 
 import com.heartz.byeboo.domain.model.User;
+import com.heartz.byeboo.domain.type.EPlatform;
+
+import java.util.Optional;
 
 public interface RetrieveUserPort {
     User getUserById(Long userId);
+    Optional<User> findUserByPlatFormAndSeralId(EPlatform platform, String serialId);
 }
--- src/main/java/com/heartz/byeboo/mapper/UserMapper.java
+++ src/main/java/com/heartz/byeboo/mapper/UserMapper.java
@@ -3,13 +3,18 @@ package com.heartz.byeboo.mapper;
 import com.heartz.byeboo.adapter.out.persistence.entity.UserEntity;
 import com.heartz.byeboo.application.command.user.UserCreateCommand;
 import com.heartz.byeboo.domain.model.User;
+import com.heartz.byeboo.domain.type.EPlatform;
+import com.heartz.byeboo.domain.type.ERole;
 
 public class UserMapper {
     public static UserEntity toEntity(User user) {
         return UserEntity.create(
                 user.getName(),
                 user.getQuestStyle(),
-                user.getCurrentNumber()
+                user.getCurrentNumber(),
+                user.getPlatform(),
+                user.getRole(),
+                user.getSerialId()
         );
     }
 
@@ -18,7 +23,10 @@ public class UserMapper {
                 userEntity.getId(),
                 userEntity.getName(),
                 userEntity.getQuestStyle(),
-                userEntity.getCurrentNumber()
+                userEntity.getCurrentNumber(),
+                userEntity.getPlatform(),
+                userEntity.getRole(),
+                userEntity.getSerialId()
         );
     }
 
@@ -36,6 +44,14 @@ public class UserMapper {
                 user.getQuestStyle(),
                 user.getCurrentNumber()
         );
+    }
+
+    public static User userInfoToDomain(String serialId, EPlatform platform, ERole role){
+        return User.builder()
+                .serialId(serialId)
+                .platform(platform)
+                .role(role)
+                .build();
     }
 
 }
