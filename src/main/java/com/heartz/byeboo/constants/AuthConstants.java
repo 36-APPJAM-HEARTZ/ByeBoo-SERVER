e52325c8c565b091240e7d46b73be4819143629c
jeong724 <oliveyou724@gmail.com>
2025-08-20 17:24:51 +0900

    feat: 상수 추가 탈퇴

--- src/main/java/com/heartz/byeboo/constants/AuthConstants.java
+++ src/main/java/com/heartz/byeboo/constants/AuthConstants.java
@@ -3,9 +3,11 @@ package com.heartz.byeboo.constants;
 import java.util.List;
 
 public class AuthConstants {
-    public static String CLAIM_USER_ROLE = "role";
-    public static String PREFIX_BEARER = "Bearer ";
-    public static List<String> NO_NEED_AUTH = List.of(
+    public static final String GRANT_TYPE = "KakaoAK ";
+    public static final String TARGET_ID_TYPE = "user_id";
+    public static final String CLAIM_USER_ROLE = "role";
+    public static final String PREFIX_BEARER = "Bearer ";
+    public static final List<String> NO_NEED_AUTH = List.of(
             "/api/health-check",
             "/api/v1/auth/login",
             "/api/v1/auth/reissue",
