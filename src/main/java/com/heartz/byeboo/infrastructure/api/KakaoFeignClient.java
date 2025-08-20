75f1475d02678c037ac71fe60934a737b9e04701
jeong724 <oliveyou724@gmail.com>
2025-08-20 17:25:03 +0900

    feat: 탈퇴 feign 메서드 추가

--- src/main/java/com/heartz/byeboo/infrastructure/api/KakaoFeignClient.java
+++ src/main/java/com/heartz/byeboo/infrastructure/api/KakaoFeignClient.java
@@ -4,15 +4,25 @@ import com.heartz.byeboo.infrastructure.dto.kakao.KakaoUserInfo;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(
         name = "kakaoClient",
         url = "https://kapi.kakao.com")
 public interface KakaoFeignClient {
+
     @GetMapping("/v2/user/me")
     KakaoUserInfo getUserInfo(
             @RequestHeader(HttpHeaders.AUTHORIZATION) String accessToken
+    );
+
+    @PostMapping("/v1/user/unlink")
+    void unlinkKakaoServer(
+            @RequestHeader("Authorization") String adminKey,
+            @RequestParam(name = "target_id_type") String targetIdType,
+            @RequestParam("target_id") Long targetId
     );
 
 }
