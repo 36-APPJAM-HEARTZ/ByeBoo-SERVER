da6e074af9a912846bc5f79c18f10243c417360e
jeong724 <oliveyou724@gmail.com>
2025-08-21 02:38:26 +0900

    feat: 회원 탈퇴 서비스 추가

--- src/main/java/com/heartz/byeboo/adapter/out/OAuthUserInfoAdapter.java
+++ src/main/java/com/heartz/byeboo/adapter/out/OAuthUserInfoAdapter.java
@@ -1,6 +1,7 @@
 package com.heartz.byeboo.adapter.out;
 
-import com.heartz.byeboo.application.port.out.RetrieveUserInfoPort;
+import com.heartz.byeboo.application.port.out.userinfo.RetrieveUserInfoPort;
+import com.heartz.byeboo.application.port.out.userinfo.UnlinkUserInfoPort;
 import com.heartz.byeboo.domain.type.EPlatform;
 import com.heartz.byeboo.infrastructure.dto.SocialInfoResponse;
 import com.heartz.byeboo.security.service.OAuthProvider;
@@ -10,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
 @Component
-public class OAuthUserInfoAdapter implements RetrieveUserInfoPort {
+public class OAuthUserInfoAdapter implements RetrieveUserInfoPort, UnlinkUserInfoPort {
 
     private final OAuthProviderFactory oAuthProviderFactory;
 
@@ -18,5 +19,11 @@ public class OAuthUserInfoAdapter implements RetrieveUserInfoPort {
     public SocialInfoResponse getUserInfo(String token, EPlatform platform) {
         OAuthProvider oAuthProvider = oAuthProviderFactory.findProvider(platform);
         return oAuthProvider.getUserInfo(token);
+    }
+
+    @Override
+    public void revoke(EPlatform platform, String code, String serialId) {
+        OAuthProvider oAuthProvider = oAuthProviderFactory.findProvider(platform);
+        oAuthProvider.requestRevoke(code, serialId);
     }
 }
--- src/main/java/com/heartz/byeboo/security/service/OAuthService.java
+++ src/main/java/com/heartz/byeboo/security/service/OAuthService.java
@@ -5,8 +5,10 @@ import com.heartz.byeboo.application.port.in.dto.response.auth.UserInfoResponse;
 import com.heartz.byeboo.application.port.in.dto.response.auth.UserLoginResponse;
 import com.heartz.byeboo.application.port.out.user.CreateUserPort;
 import com.heartz.byeboo.application.port.out.user.RetrieveUserPort;
+import com.heartz.byeboo.application.port.out.user.UpdateUserPort;
 import com.heartz.byeboo.domain.model.User;
 import com.heartz.byeboo.domain.type.ERole;
+import com.heartz.byeboo.domain.type.EUserStatus;
 import com.heartz.byeboo.infrastructure.dto.SocialInfoResponse;
 import com.heartz.byeboo.mapper.UserMapper;
 import com.heartz.byeboo.security.command.OAuthLoginCommand;
@@ -22,6 +24,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
+import static com.heartz.byeboo.domain.type.EUserStatus.ACTIVE;
+
 @Service
 @RequiredArgsConstructor
 @Slf4j
@@ -31,14 +35,16 @@ public class OAuthService implements OAuthUseCase {
     private final RetrieveUserPort retrieveUserPort;
     private final CreateUserPort createUserPort;
     private final JwtProvider jwtProvider;
+    private final UpdateUserPort updateUserPort;
 
 
     @Transactional
+    @Override
     public UserLoginResponse login(OAuthLoginCommand command) {
         SocialInfoResponse response = oAuthUserInfoAdapter.getUserInfo(command.getToken(), command.getPlatform());
         UserInfoResponse userInfoResponse = UserInfoResponse.of(command.getPlatform(), response.serialId());
         Optional<User> user = retrieveUserPort.findUserByPlatFormAndSeralId(userInfoResponse.platform(), userInfoResponse.serialId());
-        boolean isRegistered = user.isPresent();
+        boolean isRegistered = isRegistered(user);
         User findUser = loadOrCreateUser(user, userInfoResponse);
         Token issuedToken = jwtProvider.issueTokens(findUser.getId(), getUserRole(findUser.getId()));
         return UserLoginResponse.of(issuedToken, isRegistered);
@@ -53,15 +59,40 @@ public class OAuthService implements OAuthUseCase {
     }
 
     @Override
+    @Transactional
     public Void withdraw(OAuthWithdrawCommand command) {
         User findUser = retrieveUserPort.getUserById(command.userId());
-
+        oAuthUserInfoAdapter.revoke(findUser.getPlatform(), command.code(), findUser.getSerialId());
+        findUser.softDelete();
+        updateUserPort.updateUser(findUser);
         return null;
+    }
+
+    private boolean isRegistered(final Optional<User> user) {
+        return user.map(u -> u.getStatus() == ACTIVE)
+                .orElse(false);
     }
 
     private User loadOrCreateUser(final Optional<User> findUser, final UserInfoResponse userInfo) {
         return findUser.orElseGet(() -> createNewUser(userInfo));
     }
+
+//    private User loadOrCreateUser(final Optional<User> findUser, final UserInfoResponse userInfo) {
+//        return findUser.map(user -> updateOrGetUserInfo(user, user.getStatus(), userInfo))
+//                .orElseGet(() -> createNewUser(userInfo));
+//    }
+//
+//    private User updateOrGetUserInfo(final User user, final EUserStatus status, final UserInfoResponse userInfo) {
+//        if (status == ACTIVE) {
+//            return user;
+//        }
+//        return updateUserInfo(user, userInfo);
+//    }
+//
+//    private User updateUserInfo(final User user) {
+//        user.rejoin();
+//        return user;
+//    }
 
     private User createNewUser(final UserInfoResponse userInfo) {
         User newUser = UserMapper.userInfoToDomain(userInfo.serialId(), userInfo.platform(), ERole.USER);
