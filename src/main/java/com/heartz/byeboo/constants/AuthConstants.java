12b5910af6246a4e3da3c7998cea1b017273b201
jeong724 <oliveyou724@gmail.com>
2025-08-21 02:37:30 +0900

    feat: 상수 추가

--- src/main/java/com/heartz/byeboo/constants/AuthConstants.java
+++ src/main/java/com/heartz/byeboo/constants/AuthConstants.java
@@ -7,6 +7,7 @@ public class AuthConstants {
     public static final String TARGET_ID_TYPE = "user_id";
     public static final String CLAIM_USER_ROLE = "role";
     public static final String PREFIX_BEARER = "Bearer ";
+    public static final String DELETED_USER_DEFAULT_INFO = "알 수 없음";
     public static final List<String> NO_NEED_AUTH = List.of(
             "/api/health-check",
             "/api/v1/auth/login",
