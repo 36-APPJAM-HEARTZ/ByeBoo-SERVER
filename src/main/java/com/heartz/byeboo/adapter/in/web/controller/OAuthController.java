e1550dc05ad3d029ef881ee206bf6b9c93ab3d4d
jeong724 <oliveyou724@gmail.com>
2025-08-20 17:25:46 +0900

    feat: 탈퇴, 로그아웃 api 추가

--- src/main/java/com/heartz/byeboo/adapter/in/web/controller/OAuthController.java
+++ src/main/java/com/heartz/byeboo/adapter/in/web/controller/OAuthController.java
@@ -3,8 +3,11 @@ package com.heartz.byeboo.adapter.in.web.controller;
 import com.heartz.byeboo.adapter.in.web.dto.request.UserLoginRequestDto;
 import com.heartz.byeboo.application.port.in.dto.response.auth.UserLoginResponse;
 import com.heartz.byeboo.constants.AuthConstants;
+import com.heartz.byeboo.core.annotation.UserId;
 import com.heartz.byeboo.core.common.BaseResponse;
-import com.heartz.byeboo.security.command.OAuthCommand;
+import com.heartz.byeboo.security.command.OAuthLoginCommand;
+import com.heartz.byeboo.security.command.OAuthLogoutCommand;
+import com.heartz.byeboo.security.command.OAuthWithdrawCommand;
 import com.heartz.byeboo.security.usecase.OAuthUseCase;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -12,6 +15,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
+import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -49,6 +53,21 @@ public class OAuthController {
             @Parameter(hidden = true) @RequestHeader(HttpHeaders.AUTHORIZATION) final String token,
             @RequestBody final UserLoginRequestDto request){
         String rawToken = token.substring(AuthConstants.PREFIX_BEARER.length()).trim();
-        return BaseResponse.success(oAuthUseCase.login(OAuthCommand.of(request.platform(), rawToken)));
+        return BaseResponse.success(oAuthUseCase.login(OAuthLoginCommand.of(request.platform(), rawToken)));
     }
+
+    @DeleteMapping("/auth/logout")
+    public BaseResponse<Void> signOut(
+            @UserId final Long userId
+    ) {
+        oAuthUseCase.logout(OAuthLogoutCommand.from(userId));
+        return BaseResponse.success(oAuthUseCase.logout(OAuthLogoutCommand.from(userId)));
+    }
+
+    @DeleteMapping("/auth/withdraw")
+    public BaseResponse<Void> withdraw(@UserId final Long userId,
+                                         @Nullable @RequestHeader("X-Apple-Code") final String code) {
+        return BaseResponse.success(oAuthUseCase.withdraw(OAuthWithdrawCommand.of(userId, code)));
+    }
+
 }
