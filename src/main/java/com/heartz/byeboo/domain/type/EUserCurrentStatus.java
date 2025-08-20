0b803a791d5412f41cd006bea17b0a9c5b77e662
ssangbaam <tkdqja2196@naver.com>
2025-08-20 15:25:13 +0900

    feat: 유저 현재 상태 enum 값 추가

---
+++ src/main/java/com/heartz/byeboo/domain/type/EUserCurrentStatus.java
@@ -0,0 +1,15 @@
+package com.heartz.byeboo.domain.type;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public enum EUserCurrentStatus {
+    INITIAL_START_STATUS("여정을 시작하지 않은 상태"),
+    TODAY_NOT_COMPLETED_STATUS("오늘의 퀘스트 완료 전"),
+    TODAY_COMPLETED_STATUS("오늘의 퀘스트 완료 후"),
+    JOURNEY_COMPLETED("여정 완료 후 & 새로운 여정 시작 전");
+
+    private final String label;
+}
