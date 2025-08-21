a50b8fa0324fb9fff3b5ec9a4f3ef5d3bd2bf319
jeong724 <oliveyou724@gmail.com>
2025-08-21 11:21:27 +0900

    feat: 상수와 에러 코드 추가

--- src/main/java/com/heartz/byeboo/domain/exception/AuthErrorCode.java
+++ src/main/java/com/heartz/byeboo/domain/exception/AuthErrorCode.java
@@ -9,6 +9,12 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum AuthErrorCode implements ErrorCode {
 
+    UNSUPPORTED_NULL_NAME(HttpStatus.BAD_REQUEST, "요청 값에 이름이 누락되었습니다"),
+    UNSUPPORTED_ALGORITHM(HttpStatus.BAD_REQUEST, "키 생성에 사용된 알고리즘을 지원하지 않습니다: "),
+    INVALID_KEY_SPEC(HttpStatus.BAD_REQUEST, "공개 키 생성에 잘못된 키 사양이 제공되었습니다."),
+    FAILED_TO_LOAD_PRIVATE_KEY(HttpStatus.BAD_REQUEST, "개인 키를 로드하는 데 실패했습니다."),
+    APPLE_REVOKE_FAILED(HttpStatus.BAD_REQUEST, "Apple 탈퇴에 실패했습니다."),
+
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "리소스 접근 권한이 없습니다."),
     INVALID_PLATFORM(HttpStatus.UNAUTHORIZED,"올바르지 않은 로그인 방식입니다."),
     INVALID_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "올바르지 않은 토큰 형식입니다."),
@@ -17,7 +23,10 @@ public enum AuthErrorCode implements ErrorCode {
     INVALID_REFRESH_TOKEN_VALUE(HttpStatus.UNAUTHORIZED, "리프레시 토큰의 값이 올바르지 않습니다."),
     EXPIRED_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "리프레시 토큰이 만료되었습니다."),
     MISMATCH_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "리프레시 토큰이 일치하지 않습니다."),
-    MISSING_BEARER_PREFIX(HttpStatus.UNAUTHORIZED, "Bearer가 누락되었습니다.");
+    MISSING_BEARER_PREFIX(HttpStatus.UNAUTHORIZED, "Bearer가 누락되었습니다."),
+    INVALID_APPLE_IDENTITY_TOKEN(HttpStatus.UNAUTHORIZED, "애플 아이덴티티 토큰이 유효하지 않습니다."),
+    EXPIRED_IDENTITY_TOKEN(HttpStatus.UNAUTHORIZED, "아이덴티티 토큰이 만료되었습니다."),
+    INVALID_IDENTITY_TOKEN_VALUE(HttpStatus.UNAUTHORIZED, "애플 아이덴티티 토큰의 값이 올바르지 않습니다.");
 
 
     private final HttpStatus status;
