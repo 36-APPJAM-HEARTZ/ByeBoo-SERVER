a7be0f751c6c197189cf56c9cfc5f1921e8de872
ssangbaam <tkdqja2196@naver.com>
2025-08-31 23:10:04 +0900

    feat: 로그인 응답에 유저 정보 추가

--- src/main/java/com/heartz/byeboo/application/port/in/dto/response/auth/UserLoginResponse.java
+++ src/main/java/com/heartz/byeboo/application/port/in/dto/response/auth/UserLoginResponse.java
@@ -1,5 +1,6 @@
 package com.heartz.byeboo.application.port.in.dto.response.auth;
 
+import com.heartz.byeboo.domain.type.EJourney;
 import com.heartz.byeboo.security.jwt.TokenResponse;
 
 import java.io.Serializable;
@@ -7,9 +8,17 @@ import java.io.Serializable;
 public record UserLoginResponse(
         String accessToken,
         String refreshToken,
-        boolean isRegistered
+        boolean isRegistered,
+        String name,
+        EJourney journey
 ) implements Serializable {
-    public static UserLoginResponse of(TokenResponse tokenResponse, boolean isRegistered){
-        return new UserLoginResponse(tokenResponse.accessToken(), tokenResponse.refreshToken(), isRegistered);
+    public static UserLoginResponse of(TokenResponse tokenResponse, boolean isRegistered, String name, EJourney journey){
+        return new UserLoginResponse(
+                tokenResponse.accessToken(),
+                tokenResponse.refreshToken(),
+                isRegistered,
+                name,
+                journey
+        );
     }
 }
