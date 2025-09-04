720bebf9b3e9dfe6323b43b6a6c75fbbf921fd92
jeong724 <oliveyou724@gmail.com>
2025-09-04 15:33:29 +0900

    feat: 파라미터 수정

--- src/main/java/com/heartz/byeboo/application/service/auth/OAuthProvider.java
+++ src/main/java/com/heartz/byeboo/application/service/auth/OAuthProvider.java
@@ -1,10 +1,11 @@
 package com.heartz.byeboo.application.service.auth;
 
+import com.heartz.byeboo.application.command.auth.ProviderUserInfoCommand;
 import com.heartz.byeboo.infrastructure.dto.SocialInfoResponse;
 
 
 public interface OAuthProvider {
 
-    SocialInfoResponse getUserInfo(final String providerToken);
-    void requestRevoke(String code, String serialId);
+    SocialInfoResponse getUserInfo(ProviderUserInfoCommand command);
+    void requestRevoke(String refreshToken, String serialId);
 }
