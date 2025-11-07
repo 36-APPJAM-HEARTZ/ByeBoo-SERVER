d15eedf09008608ecb57f9a24af595914ae594f9
jeong724 <oliveyou724@gmail.com>
2025-11-08 04:34:05 +0900

    feat : fcm 메시지 상수 추가

---
+++ src/main/java/com/heartz/byeboo/constants/NotificationConstants.java
@@ -0,0 +1,7 @@
+package com.heartz.byeboo.constants;
+
+public class NotificationConstants {
+
+    public static final String notificationTitle = "보리의 퀘스트 선물";
+    public static final String notificationBody = "이별 극복을 위한 한 걸음, 보리와 함께 해봐요.";
+}
