93474f2c5f62409de0c033aca0e6972837d1b5c9
jeong724 <oliveyou724@gmail.com>
2025-11-08 04:35:11 +0900

    feat : fcm 엔티티 추가

--- src/main/java/com/heartz/byeboo/adapter/out/persistence/entity/NotificationTokenEntity.java
+++ src/main/java/com/heartz/byeboo/adapter/out/persistence/entity/NotificationTokenEntity.java
@@ -1,5 +1,6 @@
 package com.heartz.byeboo.adapter.out.persistence.entity;
 
+import com.heartz.byeboo.domain.model.NotificationToken;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -12,7 +13,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @Table(name = "fcm_token")
-public class TokenFCMEntity {
+public class NotificationTokenEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -29,15 +30,25 @@ public class TokenFCMEntity {
     private Long userId;
 
     @Builder
-    public TokenFCMEntity(String fcmToken, LocalDateTime timeStamp, Long userId) {
+    public NotificationTokenEntity(Long id, String fcmToken, LocalDateTime timeStamp, Long userId) {
         this.fcmToken = fcmToken;
         this.userId = userId;
         this.timeStamp = timeStamp;
+        this.id = id;
     }
 
-    public static TokenFCMEntity create(String fcmToken, LocalDateTime timeStamp, Long userId) {
-        return TokenFCMEntity.builder()
+    public static NotificationTokenEntity create(String fcmToken, LocalDateTime timeStamp, Long userId) {
+        return NotificationTokenEntity.builder()
                 .fcmToken(fcmToken)
+                .timeStamp(timeStamp)
+                .userId(userId)
+                .build();
+    }
+
+    public static NotificationTokenEntity createForUpdate(Long id,String fcmToken, LocalDateTime timeStamp, Long userId){
+        return NotificationTokenEntity.builder()
+                .fcmToken(fcmToken)
+                .id(id)
                 .timeStamp(timeStamp)
                 .userId(userId)
                 .build();
