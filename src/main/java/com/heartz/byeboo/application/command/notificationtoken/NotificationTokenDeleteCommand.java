b599761c14daaa2e7e297d7f096fb83418ea686d
jeong724 <oliveyou724@gmail.com>
2025-11-08 04:34:42 +0900

    feat : fcm command 추가

---
+++ src/main/java/com/heartz/byeboo/application/command/notificationtoken/NotificationTokenDeleteCommand.java
@@ -0,0 +1,16 @@
+package com.heartz.byeboo.application.command.notificationtoken;
+
+import lombok.Builder;
+
+@Builder
+public record NotificationTokenDeleteCommand(
+        Long userId,
+        String notificationToken
+) {
+    public static NotificationTokenDeleteCommand from(Long userId, String notificationToken){
+        return NotificationTokenDeleteCommand.builder()
+                .notificationToken(notificationToken)
+                .userId(userId)
+                .build();
+    }
+}
