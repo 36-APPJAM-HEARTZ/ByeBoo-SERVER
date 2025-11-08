39fb14fc5594ce8a148f165aba4e6dc290704475
jeong724 <oliveyou724@gmail.com>
2025-11-09 00:03:39 +0900

    feat : fcm 발송 스케줄러 추가

--- src/main/java/com/heartz/byeboo/application/service/userquest/QuestNotificationScheduler.java
+++ src/main/java/com/heartz/byeboo/application/service/userquest/QuestNotificationScheduler.java
@@ -1,40 +1,19 @@
 package com.heartz.byeboo.application.service.userquest;
 
-import com.heartz.byeboo.adapter.out.FCMNotificationPersistenceAdapter;
-import com.heartz.byeboo.adapter.out.FCMTokenPersistenceAdapter;
-import com.heartz.byeboo.adapter.out.persistence.entity.UserQuestEntity;
-import com.heartz.byeboo.adapter.out.persistence.repository.NotificationTokenRepository;
-import com.heartz.byeboo.adapter.out.persistence.repository.UserQuestRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
 @Component
 @RequiredArgsConstructor
 public class QuestNotificationScheduler {
-    private final UserQuestRepository userQuestRepository;
-    private final NotificationTokenRepository notificationTokenRepository;
-    private final FCMNotificationPersistenceAdapter fCMNotificationPersistenceAdapter;
+
+    private final UserQuestService userQuestService;
 
     @Scheduled(cron = "0 */1 * * * *", zone = "Asia/Seoul") // 매 1분마다 실행
     @Transactional
     public void sendQuestNotifications() {
-        LocalDateTime threshold = LocalDateTime.now().minusHours(24);
-        List<UserQuestEntity> quests = userQuestRepository.findUnnotifiedQuestsBefore(threshold);
-
-        for (UserQuestEntity quest : quests) {
-            notificationTokenRepository.findByUserId(quest.getUserId()).ifPresent(token -> {
-                fCMNotificationPersistenceAdapter.sendMessage(
-                        token.getFcmToken()
-                );
-                quest.updateNotified(true);
-            });
-        }
+        userQuestService.sendQuestNotifications();
     }
-
-
 }
