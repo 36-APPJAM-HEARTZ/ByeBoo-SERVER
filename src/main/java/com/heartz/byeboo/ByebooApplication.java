82052a1dc956762663d3b2116ba2b5f3c7d11fb2
jeong724 <oliveyou724@gmail.com>
2025-11-09 22:26:46 +0900

    feat : 스케줄러 enable

--- src/main/java/com/heartz/byeboo/ByebooApplication.java
+++ src/main/java/com/heartz/byeboo/ByebooApplication.java
@@ -3,8 +3,10 @@ package com.heartz.byeboo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ByebooApplication {
 
 	public static void main(String[] args) {
