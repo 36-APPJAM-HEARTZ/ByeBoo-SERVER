1340fc187bb2943f9e4ce013fdddea016efe7559
ssangbaam <tkdqja2196@naver.com>
2025-11-13 00:52:49 +0900

    feat: userQuest 수정 port 추가

---
+++ src/main/java/com/heartz/byeboo/application/port/out/userquest/UpdateUserQuestPort.java
@@ -0,0 +1,7 @@
+package com.heartz.byeboo.application.port.out.userquest;
+
+import com.heartz.byeboo.domain.model.UserQuest;
+
+public interface UpdateUserQuestPort {
+    void updateUserQuest(UserQuest userQuest);
+}
