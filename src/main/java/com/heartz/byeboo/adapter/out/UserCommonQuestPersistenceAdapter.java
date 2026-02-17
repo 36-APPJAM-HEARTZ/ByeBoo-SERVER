ea830c82c47749d9b077f022d4f63c3645f6b4a4
jeong724 <oliveyou724@gmail.com>
2026-02-18 02:45:55 +0900

    feat: 공통 퀘스트 삭제 레포지토리 메서드 추가

--- src/main/java/com/heartz/byeboo/adapter/out/UserCommonQuestPersistenceAdapter.java
+++ src/main/java/com/heartz/byeboo/adapter/out/UserCommonQuestPersistenceAdapter.java
@@ -4,6 +4,9 @@ import com.heartz.byeboo.adapter.out.persistence.entity.UserCommonQuestEntity;
 import com.heartz.byeboo.adapter.out.persistence.repository.UserCommonQuestRepository;
 import com.heartz.byeboo.application.port.out.usercommonquest.CreateUserCommonQuestPort;
 import com.heartz.byeboo.application.port.out.usercommonquest.RetrieveUserCommonQuestPort;
+import com.heartz.byeboo.core.exception.CustomException;
+import com.heartz.byeboo.domain.exception.UserCommonQuestErrorCode;
+import com.heartz.byeboo.domain.model.CommonQuest;
 import com.heartz.byeboo.domain.model.User;
 import com.heartz.byeboo.domain.model.UserCommonQuest;
 import com.heartz.byeboo.mapper.UserCommonQuestMapper;
@@ -32,5 +35,14 @@ public class UserCommonQuestPersistenceAdapter implements CreateUserCommonQuestP
         LocalDateTime endOfToday = LocalDate.now().atTime(LocalTime.MAX);
 
         return userCommonQuestRepository.existsByUserIdAndCreatedDateBetween(user.getId(), startOfToday, endOfToday);
+    }
+
+    @Override
+    public void deleteByUserIdAndId(Long userCommonQuestId, User user) {
+        long deletedCount = userCommonQuestRepository.deleteByUserIdAndId(user.getId(), userCommonQuestId);
+
+        if (deletedCount == 0) {
+            throw new CustomException(UserCommonQuestErrorCode.USER_COMMON_QUEST_NOT_FOUND);
+        }
     }
 }
--- src/main/java/com/heartz/byeboo/adapter/out/persistence/repository/UserCommonQuestRepository.java
+++ src/main/java/com/heartz/byeboo/adapter/out/persistence/repository/UserCommonQuestRepository.java
@@ -5,7 +5,8 @@ import com.heartz.byeboo.domain.model.UserCommonQuest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 public interface UserCommonQuestRepository extends JpaRepository<UserCommonQuestEntity, Long> {
     boolean existsByUserIdAndCreatedDateBetween(Long userId, LocalDateTime start, LocalDateTime end);
-}
+    long deleteByUserIdAndId(Long userId, Long id);}
