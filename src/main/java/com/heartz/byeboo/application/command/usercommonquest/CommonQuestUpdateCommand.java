61991df590a65946afebb8a5746ab0e8c68cfbe8
jeong724 <oliveyou724@gmail.com>
2026-02-19 02:12:44 +0900

    feat: 글자수 예외처리 추가

--- src/main/java/com/heartz/byeboo/application/command/usercommonquest/CommonQuestUpdateCommand.java
+++ src/main/java/com/heartz/byeboo/application/command/usercommonquest/CommonQuestUpdateCommand.java
@@ -1,6 +1,10 @@
 package com.heartz.byeboo.application.command.usercommonquest;
 
 import com.heartz.byeboo.adapter.in.web.dto.request.CommonQuestUpdateRequestDto;
+import com.heartz.byeboo.constants.QuestConstants;
+import com.heartz.byeboo.core.exception.CustomException;
+import com.heartz.byeboo.domain.exception.UserCommonQuestErrorCode;
+import com.heartz.byeboo.utils.TextUtil;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -12,10 +16,22 @@ public class CommonQuestUpdateCommand {
     private String answer;
 
     public static CommonQuestUpdateCommand from(CommonQuestUpdateRequestDto commonQuestUpdateRequestDto, Long answerId, Long userId){
+        validateAnswerLength(commonQuestUpdateRequestDto.answer());
+
         return CommonQuestUpdateCommand.builder()
                 .answerId(answerId)
                 .userId(userId)
                 .answer(commonQuestUpdateRequestDto.answer())
                 .build();
+    }
+
+    private static void validateAnswerLength(String answer){
+        if (TextUtil.lengthWithEmoji(answer) > QuestConstants.COMMON_QUEST_MAX){
+            throw new CustomException(UserCommonQuestErrorCode.COMMON_QUEST_TOO_LONG);
+        }
+
+        if (TextUtil.lengthWithEmoji(answer) < QuestConstants.COMMON_QUEST_MIN){
+            throw new CustomException(UserCommonQuestErrorCode.COMMON_QUEST_TOO_SHORT);
+        }
     }
 }
