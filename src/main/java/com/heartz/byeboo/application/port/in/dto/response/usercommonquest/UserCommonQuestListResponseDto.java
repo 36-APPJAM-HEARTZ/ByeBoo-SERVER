b1ca150a33ac936e8473fa1e9ab218573a92aa53
jeong724 <oliveyou724@gmail.com>
2026-02-19 18:56:58 +0900

    feat: 날짜별 퀘스트 조회 API dto

---
+++ src/main/java/com/heartz/byeboo/application/port/in/dto/response/usercommonquest/UserCommonQuestListResponseDto.java
@@ -0,0 +1,26 @@
+package com.heartz.byeboo.application.port.in.dto.response.usercommonquest;
+
+import lombok.Builder;
+
+import java.util.List;
+
+@Builder
+public record UserCommonQuestListResponseDto(
+        String question,
+        boolean isAnswered,
+        int answerCount,
+        List<UserCommonQuestDetailResponseDto> answers,
+        boolean hasNext,
+        Long nextCursor
+) {
+    public static UserCommonQuestListResponseDto from(String question, boolean isAnswered, int answerCount, List<UserCommonQuestDetailResponseDto> answers, boolean hasNext, Long nextCursor){
+        return UserCommonQuestListResponseDto.builder()
+                .answerCount(answerCount)
+                .answers(answers)
+                .isAnswered(isAnswered)
+                .question(question)
+                .hasNext(hasNext)
+                .nextCursor(nextCursor)
+                .build();
+    }
+}
