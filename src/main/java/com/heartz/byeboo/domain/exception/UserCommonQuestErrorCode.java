1d9b5bf6ed131afe6b1039fafd3563d94424a229
jeong724 <oliveyou724@gmail.com>
2026-02-20 02:10:01 +0900

    feat : 공통퀘스트 상세 조회 응답 dto 추가

--- src/main/java/com/heartz/byeboo/domain/exception/UserCommonQuestErrorCode.java
+++ src/main/java/com/heartz/byeboo/domain/exception/UserCommonQuestErrorCode.java
@@ -12,7 +12,7 @@ public enum UserCommonQuestErrorCode implements ErrorCode {
     COMMON_QUEST_TOO_SHORT(HttpStatus.BAD_REQUEST, "공통 퀘스트 답변 길이는 10자 이상입니다."),
     COMMON_QUEST_NOT_TODAY(HttpStatus.BAD_REQUEST, "작성 가능한 날이 아닙니다."),
     COMMON_QUEST_ALREADY_EXIST(HttpStatus.CONFLICT, "이미 작성 완료 했습니다."),
-    USER_COMMON_QUEST_NOT_FOUND(HttpStatus.NOT_FOUND, "유저 퀘스트가 존재하지 않습니다."),
+    USER_COMMON_QUEST_NOT_FOUND(HttpStatus.NOT_FOUND, "유저 공통 퀘스트가 존재하지 않습니다."),
     USER_COMMON_QUEST_NOT_FUTURE(HttpStatus.BAD_REQUEST, "오늘 날짜 이후의 날짜에 대해서는 조회할 수 없습니다.")
     ;
 
