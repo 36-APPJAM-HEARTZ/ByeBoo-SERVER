9e3973df827aad181cb292fae6d52fa28748ca95
jeong724 <oliveyou724@gmail.com>
2026-02-21 02:23:48 +0900

    feat : 비즈니스 메서드

--- src/main/java/com/heartz/byeboo/application/port/in/usecase/ReportUseCase.java
+++ src/main/java/com/heartz/byeboo/application/port/in/usecase/ReportUseCase.java
@@ -1,5 +1,7 @@
 package com.heartz.byeboo.application.port.in.usecase;
 
+import com.heartz.byeboo.application.command.report.CommonQuestReportCreateCommand;
+
 public interface ReportUseCase {
-    Void reportCommonQuest(Long userId);
+    Void reportCommonQuest(CommonQuestReportCreateCommand command);
 }
