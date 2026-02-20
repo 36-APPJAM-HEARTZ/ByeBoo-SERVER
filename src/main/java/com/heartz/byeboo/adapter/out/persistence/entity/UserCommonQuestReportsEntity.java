eddcf29a738ce84628b4daa88ef1333c95c34038
jeong724 <oliveyou724@gmail.com>
2026-02-21 01:32:48 +0900

    feat : 게시글 신고 엔티티 추가

---
+++ src/main/java/com/heartz/byeboo/adapter/out/persistence/entity/UserCommonQuestReportsEntity.java
@@ -0,0 +1,56 @@
+package com.heartz.byeboo.adapter.out.persistence.entity;
+
+import com.heartz.byeboo.domain.type.EQuestStyle;
+import com.heartz.byeboo.domain.type.EReportStatus;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "user_common_quest_reports")
+public class UserCommonQuestReportsEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "report_status")
+    private EReportStatus reportStatus;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Column(name = "user_common_quest_id", nullable = false)
+    private Long userCommonQuestId;
+
+    @Builder
+    public UserCommonQuestReportsEntity(
+            Long id,
+            EReportStatus reportStatus,
+            Long userId,
+            Long userCommonQuestId
+    ) {
+        this.id = id;
+        this.reportStatus = reportStatus;
+        this.userId = userId;
+        this.userCommonQuestId = userCommonQuestId;
+    }
+
+    public static UserCommonQuestReportsEntity create(
+            Long userId,
+            Long userCommonQuestId,
+            EReportStatus reportStatus
+    ) {
+        return UserCommonQuestReportsEntity.builder()
+                .userId(userId)
+                .userCommonQuestId(userCommonQuestId)
+                .reportStatus(reportStatus)
+                .build();
+    }
+}
