28b9be8c0d62966c71d8a1ef946980e7aceb30fa
jeong724 <oliveyou724@gmail.com>
2026-02-23 03:00:44 +0900

    rename : UserCommonQuestReportEntity 이름 수정

--- src/main/java/com/heartz/byeboo/application/port/out/report/CreateReportPort.java
+++ src/main/java/com/heartz/byeboo/application/port/out/report/CreateReportPort.java
@@ -1,8 +1,8 @@
 package com.heartz.byeboo.application.port.out.report;
 
-import com.heartz.byeboo.adapter.out.persistence.entity.UserCommonQuestReportsEntity;
+import com.heartz.byeboo.adapter.out.persistence.entity.UserCommonQuestReportEntity;
 import com.heartz.byeboo.domain.model.UserCommonQuestReport;
 
 public interface CreateReportPort {
-    UserCommonQuestReportsEntity createReport(UserCommonQuestReport userCommonQuestReport);
+    UserCommonQuestReportEntity createReport(UserCommonQuestReport userCommonQuestReport);
 }
