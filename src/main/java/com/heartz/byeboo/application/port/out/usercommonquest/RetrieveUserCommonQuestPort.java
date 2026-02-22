ebe60c69f433a45f53cdf57205c189966f65c68e
jeong724 <oliveyou724@gmail.com>
2026-02-22 20:02:26 +0900

    refac : projection으로 반환

--- src/main/java/com/heartz/byeboo/application/port/out/usercommonquest/RetrieveUserCommonQuestPort.java
+++ src/main/java/com/heartz/byeboo/application/port/out/usercommonquest/RetrieveUserCommonQuestPort.java
@@ -1,6 +1,7 @@
 package com.heartz.byeboo.application.port.out.usercommonquest;
 
 import com.heartz.byeboo.adapter.out.persistence.repository.projection.MyCommonQuestProjection;
+import com.heartz.byeboo.adapter.out.persistence.repository.projection.UserCommonQuestInfoProjection;
 import com.heartz.byeboo.domain.model.CommonQuest;
 import com.heartz.byeboo.domain.model.User;
 import com.heartz.byeboo.domain.model.UserCommonQuest;
@@ -12,7 +13,7 @@ public interface RetrieveUserCommonQuestPort {
     boolean isUserCommonQuestExistsToday(User user);
     void deleteByUserIdAndId(Long userCommonQuestId, User user);
     UserCommonQuest getUserCommonQuestByUserAndId(User user, Long id);
-    List<UserCommonQuest> getUserCommonQuestsByCreatedDate(LocalDate targetDate, Long cursor, int limit, CommonQuest commonQuest);
+    List<UserCommonQuestInfoProjection> getUserCommonQuestsByCreatedDate(LocalDate targetDate, Long cursor, int limit, CommonQuest commonQuest);
     long countByCreatedDateBetween(LocalDate targetDate);
     UserCommonQuest getUserCommonQuestById(Long id);
     List<MyCommonQuestProjection> getMyCommonQuestsByUserId(User user, Long cursor, int limit);
