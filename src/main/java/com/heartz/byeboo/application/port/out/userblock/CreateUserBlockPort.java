6b1a460437aad87b38a83cf0f1f87c574cea6ea1
jeong724 <oliveyou724@gmail.com>
2026-02-23 03:02:13 +0900

    feat : 사용자 차단 비즈니스 로직 구현

---
+++ src/main/java/com/heartz/byeboo/application/port/out/userblock/CreateUserBlockPort.java
@@ -0,0 +1,7 @@
+package com.heartz.byeboo.application.port.out.userblock;
+
+import com.heartz.byeboo.domain.model.UserBlock;
+
+public interface CreateUserBlockPort {
+    void createUserBlock(UserBlock userBlock);
+}
