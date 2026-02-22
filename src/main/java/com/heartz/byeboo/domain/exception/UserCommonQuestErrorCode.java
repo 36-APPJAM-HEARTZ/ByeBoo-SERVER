963820b434a9f563e2acca45cbe2d623048bbabc
jeong724 <oliveyou724@gmail.com>
2026-02-22 13:23:24 +0900

    feat : 비속어 체크 에러코드 추가

--- src/main/java/com/heartz/byeboo/domain/exception/UserCommonQuestErrorCode.java
+++ src/main/java/com/heartz/byeboo/domain/exception/UserCommonQuestErrorCode.java
@@ -13,7 +13,8 @@ public enum UserCommonQuestErrorCode implements ErrorCode {
     COMMON_QUEST_NOT_TODAY(HttpStatus.BAD_REQUEST, "작성 가능한 날이 아닙니다."),
     COMMON_QUEST_ALREADY_EXIST(HttpStatus.CONFLICT, "이미 작성 완료 했습니다."),
     USER_COMMON_QUEST_NOT_FOUND(HttpStatus.NOT_FOUND, "유저 공통 퀘스트가 존재하지 않습니다."),
-    USER_COMMON_QUEST_NOT_FUTURE(HttpStatus.BAD_REQUEST, "오늘 날짜 이후의 날짜에 대해서는 조회할 수 없습니다.")
+    USER_COMMON_QUEST_NOT_FUTURE(HttpStatus.BAD_REQUEST, "오늘 날짜 이후의 날짜에 대해서는 조회할 수 없습니다."),
+    USER_COMMON_QUEST_BAD_WORDS(HttpStatus.BAD_REQUEST, "답변에 욕설을 포함하고 있습니다.")
     ;
 
     private final HttpStatus status;
