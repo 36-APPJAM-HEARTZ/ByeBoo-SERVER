b153bd96248436830e43d61df9acd142247c1baa
jeong724 <oliveyou724@gmail.com>
2026-02-25 02:10:30 +0900

    feat : 차단 사용자 정보 조회 비즈니스 로직

--- src/main/java/com/heartz/byeboo/application/port/in/usecase/UserBlockUseCase.java
+++ src/main/java/com/heartz/byeboo/application/port/in/usecase/UserBlockUseCase.java
@@ -1,7 +1,11 @@
 package com.heartz.byeboo.application.port.in.usecase;
 
 import com.heartz.byeboo.application.command.userblock.UserBlockCommand;
+import com.heartz.byeboo.application.port.in.dto.response.userblock.UserBlockListResponseDto;
+
+import java.util.List;
 
 public interface UserBlockUseCase {
     Void block(UserBlockCommand command);
+    UserBlockListResponseDto getUserBlockList(Long userId);
 }
