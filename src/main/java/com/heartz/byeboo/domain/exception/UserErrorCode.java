279e67e3a4c6dc542069a2e68164349f79cfdc58
jeong724 <oliveyou724@gmail.com>
2026-03-03 16:14:54 +0900

    feat : 에러코드 추가

--- src/main/java/com/heartz/byeboo/domain/exception/UserErrorCode.java
+++ src/main/java/com/heartz/byeboo/domain/exception/UserErrorCode.java
@@ -13,7 +13,8 @@ public enum UserErrorCode implements ErrorCode {
     INVALID_QUEST_STYLE(HttpStatus.BAD_REQUEST, "올바르지 않은 퀘스트 방식입니다."),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "유저를 찾을 수 없습니다."),
     ALREADY_PROCEED_ONBOARDING(HttpStatus.BAD_REQUEST, "이미 온보딩을 진행한 유저 입니다."),
-    ADMIN_LOGIN_ERROR(HttpStatus.BAD_REQUEST, "관리자 ID 또는 PW가 일치하지 않습니다.")
+    ADMIN_LOGIN_ERROR(HttpStatus.BAD_REQUEST, "관리자 ID 또는 PW가 일치하지 않습니다."),
+    INVALID_REQUEST_PARAMETER(HttpStatus.BAD_REQUEST, "해당 퀘스트 방식은 더 이상 지원하지 않습니다.")
     ;
 
     private final HttpStatus status;
