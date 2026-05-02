24b67e694f9573202d556290737da0a378121ab2
jeong724 <oliveyou724@gmail.com>
2026-05-02 18:29:47 +0900

    feat : 댓글 레포지토리 메서드 구현

--- src/main/java/com/heartz/byeboo/adapter/out/persistence/repository/CommentRepository.java
+++ src/main/java/com/heartz/byeboo/adapter/out/persistence/repository/CommentRepository.java
@@ -1,7 +1,11 @@
 package com.heartz.byeboo.adapter.out.persistence.repository;
 
 import com.heartz.byeboo.adapter.out.persistence.entity.CommentEntity;
+import com.heartz.byeboo.domain.model.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
+    Optional<CommentEntity> findByUserIdAndId(Long userId, Long id);
 }
