2041c9612095595db845eec2897732d8b33a2936
jeong724 <oliveyou724@gmail.com>
2026-05-02 14:12:18 +0900

    feat : 댓글 생성 api 추가

---
+++ src/main/java/com/heartz/byeboo/application/port/out/comment/CreateCommentPort.java
@@ -0,0 +1,8 @@
+package com.heartz.byeboo.application.port.out.comment;
+
+
+import com.heartz.byeboo.domain.model.Comment;
+
+public interface CreateCommentPort {
+    void createComment(Comment comment);
+}
