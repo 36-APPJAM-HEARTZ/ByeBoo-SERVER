7955ecf2dbd6eb4ea114463879cc99a5f47ffc8a
jeong724 <oliveyou724@gmail.com>
2026-05-07 15:09:17 +0900

    feat : 신고 API V2 구현

--- src/main/java/com/heartz/byeboo/adapter/in/web/controller/ReportController.java
+++ src/main/java/com/heartz/byeboo/adapter/in/web/controller/ReportController.java
@@ -1,6 +1,8 @@
 package com.heartz.byeboo.adapter.in.web.controller;
 
+import com.heartz.byeboo.adapter.in.web.dto.request.ReportCreateRequestDto;
 import com.heartz.byeboo.application.command.report.CommonQuestReportCreateCommand;
+import com.heartz.byeboo.application.command.report.ReportCreateCommand;
 import com.heartz.byeboo.application.port.in.usecase.ReportUseCase;
 import com.heartz.byeboo.core.annotation.UserId;
 import com.heartz.byeboo.core.common.BaseResponse;
@@ -36,13 +38,40 @@ public class ReportController {
                     )
             }
     )
-    @PostMapping("/common-quests/{answerId}")
+    @PostMapping("/v1/reports/common-quests/{answerId}")
     public BaseResponse<Void> reportCommonQuest(
             @UserId final Long userId,
             @PathVariable("answerId") Long answerId
             ){
         CommonQuestReportCreateCommand commonQuestReportCreateCommand = CommonQuestReportCreateCommand.from(userId, answerId);
         return BaseResponse.success(reportUseCase.reportCommonQuest(commonQuestReportCreateCommand));
+    }
+
+    @Operation(
+            summary = "공통 퀘스트 게시글 신고 V2",
+            description = "공통 퀘스트 게시글 신고를 위한 API입니다.(V2)",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "공통 퀘스트 게시글 신고 성공"
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "존재하지 않는 유저일때"
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "서버 에러"
+                    )
+            }
+    )
+    @PostMapping("/v2/reports")
+    public BaseResponse<Void> reportCommonQuestV2(
+            @UserId final Long userId,
+            @RequestBody final ReportCreateRequestDto reportCreateRequestDto
+            ){
+        ReportCreateCommand reportCreateCommand = ReportCreateCommand.of(userId, reportCreateRequestDto);
+        return BaseResponse.success(reportUseCase.report(reportCreateCommand));
     }
 
 }
